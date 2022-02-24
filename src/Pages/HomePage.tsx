8d0aca1ff9408adaacc6edbdaf12243b04a47b42
min0k <mino.kim@minokim.com>
2022-02-24 15:25:38 -0800

    Setup the file structure

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import styles from './Homepage.module.css';
+
+export default function Homepage() {
+    return (
+        <div>
+            <h1 className={styles.h1}>hello</h1>
+        </div>
+    )
+}
