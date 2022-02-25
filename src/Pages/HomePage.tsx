9dc5152c92eb1464ce74d74f3c76537915bb3ddc
min0k <mino.kim@minokim.com>
2022-02-25 12:54:35 -0800

    Create HomePage UI
    This is an initial rough draft of the home page.

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,9 +1,26 @@
-import styles from './Homepage.module.css';
+import styles from "./HomePage.module.css";
 
-export default function Homepage() {
-    return (
-        <div>
-            <h1 className={styles.h1}>hello</h1>
-        </div>
-    )
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRunning } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function HomePage() {
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.createWorkoutContainer}>
+        <p>Pushups -- 30 seconds -- 5 reps -- 10 second breaks</p>
+        <p>2 minute break</p>
+        <p>Jumping Jacks -- 1 minute -- 10 reps -- 10 second breaks</p>
+        <p>5 minute break</p>
+        <p>Pushups -- 30 seconds -- 5 reps -- 10 second breaks</p>
+        <p>2 minute break</p>
+        <p>Jumping Jacks -- 1 minute -- 10 reps -- 10 second breaks</p>
+        <p>Done!</p>
+      </div>
+      <div className={styles.startWorkoutContainer}>
+         <FontAwesomeIcon className={styles.startIcon} icon={faRunning} />
+         <h2 className={styles.startText}>Start</h2>
+      </div>
+    </div>
+  );
 }
