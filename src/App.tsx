f2298a118ab4d46ed4a12920a8437732f7e7523f
min0k <mino.kim@minokim.com>
2022-02-24 19:30:44 -0800

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import Homepage from './Homepage';
+import HomePage from "./Pages/HomePage";
+import WorkoutPage from "./Pages/WorkoutPage";
+import FinishedPage from "./Pages/FinishedPage";
+import AboutPage from "./Pages/AboutPage";
 
 function App() {
   return (
     <div className="app">
-      <Homepage />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/workouts">My Workouts</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/workout" element={<WorkoutPage />} />
+          <Route path="/workoutdone" element={<FinishedPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
