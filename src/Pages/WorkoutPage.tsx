32718fb9aea4dea45a3666e054d06f2b3556af5b
min0k <mino.kim@minokim.com>
2022-02-25 13:02:12 -0800

    Create HomePage UI
    This is an initial rough draft of the workout page.

--- src/Pages/WorkoutPage.tsx
+++ src/Pages/WorkoutPage.tsx
@@ -1,1 +1,27 @@
-export {}
+import styles from "./WorkoutPage.module.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStopwatch} from '@fortawesome/free-solid-svg-icons'
+
+
+export default function WorkoutPage() {
+    return (
+        <div className={styles.pageContainer}>
+        <div className={styles.workoutContainer}>
+          <p>Pushups</p>
+          <p>10 second break</p>
+          <p>Pushups</p>
+          <p>10 second break</p>
+          <p>Pushups</p>
+          <p>10 second break</p>
+          <p>Pushups</p>
+          <p>10 second break</p>
+          <p>Pushups</p>
+        </div>
+        <div className={styles.countdownContainer}>
+           <FontAwesomeIcon className={styles.timerIcon} icon={faStopwatch} />
+           <h2 className={styles.timerText}>17</h2>
+        </div>
+      </div>
+    )
+}
