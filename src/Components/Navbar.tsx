5a078b5c861a677b582842ba9202d64bdecfc4f6
min0k <mino.kim@minokim.com>
2022-02-25 12:04:50 -0800

    Style navbar component

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,1 +1,19 @@
-export {}
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <h1 className={styles.h3}>Workout<span className={styles.span}>!</span></h1>
+      <nav>
+        <ul className={styles.navbarLinks}>
+            <Link className={styles.navbarItems} to="/">Home</Link>
+            <Link className={styles.navbarItems} to="/savedworkouts">My Workouts</Link>
+            <Link className={styles.navbarItems} to="/about">About</Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
