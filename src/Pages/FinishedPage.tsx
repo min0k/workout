db48eb6b19301e229c92d507919af0b245f9519f
min0k <mino.kim@minokim.com>
2022-02-25 13:09:44 -0800

    Create Finished Page UI

--- src/Pages/FinishedPage.tsx
+++ src/Pages/FinishedPage.tsx
@@ -1,1 +1,19 @@
-export {}
+import styles from "./FinishedPage.module.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function FinishedPage() {
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.workoutCompleteContainer}>
+        <h1>Complete</h1>
+        <p>Share your workout with others</p>
+      </div>
+      <div className={styles.icon}>
+         <FontAwesomeIcon className={styles.startIcon} icon={faThumbsUp} />
+      </div>
+    </div>
+  );
+}
