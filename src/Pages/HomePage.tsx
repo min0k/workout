a58739a0780dc89a6b33f7f284c109bf4a8e1174
min0k <mino.kim@minokim.com>
2022-02-27 11:48:06 -0800

    Start 'add new excercise' functionality

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,25 +1,86 @@
 import styles from "./HomePage.module.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRunning } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRunning } from "@fortawesome/free-solid-svg-icons";
 
+import React from "react";
+
+import { useNavigate } from "react-router";
+import ExcerciseCard from "../Components/ExcerciseCard";
+import IExcercise from "../Interface";
 
 export default function HomePage() {
+  const [workout, setWorkout] = React.useState<IExcercise[]>([
+    {
+      idNumber: 1,
+      excercise: "squats",
+      length: 60,
+      reps: 3,
+      rep_break: 20,
+      excercise_break: 600
+    }
+  ]);
+
+  let navigate = useNavigate();
+  
+  function handleFormInput() {
+
+  }
+
+  //Figure out how to add a new excercise and update state
+  function addNewExcercise() {
+    // setWorkout(prevValue => [
+    //   ...prevValue,
+    //   {
+    //     key: prevValue.length + 1,
+    //     excercise: "squats",
+    //     length: 60,
+    //     reps: 3,
+    //     rep_break: 20,
+    //     excercise_break: 600
+    //   }
+    // ])
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    console.log(e);
+    
+    setWorkout(prevValue => [
+      ...prevValue,
+      {
+        ...prevValue[0],
+        [e.target.name]: parseInt(e.target.value) 
+      }
+    ])
+  }
+
+//   const handleUpdateFruits = (id: string) => {
+//     setFruits((prevFruits) =>
+//       prevFruits.map((fruit) => {
+//         return fruit.id === id ? { ...fruit, inStock: !fruit.inStock } : fruit;
+//       }),
+//     );
+// };
+
+
+  const Excercises = workout.map((e, idx) => {
+    return <ExcerciseCard key={idx} idNumber={idx} rep_break={e.rep_break} reps={e.reps} length={e.length} excercise={e.excercise} excercise_break={e.excercise_break} handleChange={handleChange}/>
+  })
+
+console.log(workout);
+
+
   return (
     <div className={styles.pageContainer}>
       <div className={styles.createWorkoutContainer}>
-        <p>Pushups -- 30 seconds -- 5 reps -- 10 second breaks</p>
-        <p>2 minute break</p>
-        <p>Jumping Jacks -- 1 minute -- 10 reps -- 10 second breaks</p>
-        <p>5 minute break</p>
-        <p>Pushups -- 30 seconds -- 5 reps -- 10 second breaks</p>
-        <p>2 minute break</p>
-        <p>Jumping Jacks -- 1 minute -- 10 reps -- 10 second breaks</p>
-        <p>Done!</p>
+        {Excercises}
+        <button onClick={addNewExcercise}>Add new excercise</button>
       </div>
       <div className={styles.startWorkoutContainer}>
-         <FontAwesomeIcon className={styles.startIcon} icon={faRunning} />
-         <h2 className={styles.startText}>Start</h2>
+        <FontAwesomeIcon className={styles.startIcon} icon={faRunning} />
+        <h2 onClick={() => navigate("/workout")} className={styles.startText}>
+          Start
+        </h2>
       </div>
     </div>
   );
