774b1fd563d05a9dc6973feb0a799da1f4a8b3d2
min0k <mino.kim@minokim.com>
2022-03-05 16:25:40 -0800

    Move useWorkout state up to App component

--- src/pages/WorkoutPage.tsx
+++ src/pages/WorkoutPage.tsx
@@ -4,52 +4,40 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStopwatch } from "@fortawesome/free-solid-svg-icons";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import IExcercise from "../Interface";
 
-export default function WorkoutPage() {
+interface IProps {
+  workout: IExcercise[];
+}
+
+export default function WorkoutPage({ workout }: IProps) {
   const navigate = useNavigate();
-  const location = useLocation();
-  const state = location.state as IExcercise[];
 
   const [excerciseNumber, setExcerciseNumber] = React.useState(0);
   const [currentExcercise, setCurrentExcercise] = React.useState<IExcercise>(
-    state[excerciseNumber]
+    workout[excerciseNumber]
   );
   const [timer, setTimer] = React.useState(currentExcercise.length);
   const [breakTime, setBreakTime] = React.useState<boolean>(false);
-
-  //fence posting for my if else statements
-  // const fakeSTate = [
-  //   {
-  //     reps: 4,
-  //   },
-  //   {
-  //     reps: 7,
-  //   },
-  // ];
-  // const reps = fakeSTate[excerciseNumber].reps;
-  // console.log("Seung", reps);
 
   React.useEffect(() => {
     const startTimer = setTimeout(() => {
       if (timer !== 0) {
         setTimer((prevValue) => prevValue - 1);
       } else if (timer === 0 && breakTime === false) {
-        // Decriment rep count, set breaktime to true, set timer to rep_break.
         const newObj = { ...currentExcercise, reps: currentExcercise.reps - 1 };
         setCurrentExcercise(newObj);
         setBreakTime(true);
         setTimer(currentExcercise.rep_break);
       } else if (timer === 0 && breakTime) {
-        // Determine if last excercise
         if (currentExcercise.reps === 0) {
-          if (excerciseNumber + 1 === state.length) {
+          if (excerciseNumber + 1 === workout.length) {
             navigate("/workoutdone");
           } else {
             setTimer(currentExcercise.excercise_break);
             setExcerciseNumber((prevValue) => prevValue + 1);
-            setCurrentExcercise(state[excerciseNumber + 1]);
+            setCurrentExcercise(workout[excerciseNumber + 1]);
           }
         } else {
           setBreakTime(false);
@@ -61,10 +49,8 @@ export default function WorkoutPage() {
     return () => {
       clearTimeout(startTimer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [timer]);
-
-  console.log(state);
-  console.log(currentExcercise);
 
   return (
     <div className={styles.pageContainer}>
